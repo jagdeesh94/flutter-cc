872edc17cd63627c219b160f350693ba7dc5d31a
jagdeesh94 <aditirajendran94@gmail.com>
2020-08-01 15:38:19 +0530

    ep5 models added

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -2,24 +2,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cc/screens/location_detail/image_banner.dart';
 import 'package:flutter_cc/screens/location_detail/text_section.dart';
+import '../../models/location.dart';
 
 class LocationDetail extends StatelessWidget {
+
+  //We Shouldn't load data in build function as build function will be called multiple times.
+  //But just for demo, we have loaded here.
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    final location = locations.first;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Location Detail'),
+        title: Text(location.name),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ImageBanner("assets/images/kiyomizu-dera.jpg"),
-          TextSection("Summary 1","Something 1"),
-          TextSection("Summary 2","Something 2"),
-          TextSection("Summary 3","Something 3"),
-        ]
+          ImageBanner(location.imagePath),
+        ]..addAll(textSections(location))
       ),
     );
   }
+
+  List<Widget> textSections(Location location){
+    return location.facts.map((fact) => TextSection(fact.title,fact.text)).toList();
+  }
+
 }
